6b5e7f7b212e1e576b3cafe6028677bd72ae5654
Yarnacle <109094587+Yarnacle@users.noreply.github.com>
2024-11-09 11:18:55 -0600

    fix collections.cs for now

--- Climate Quest/Screens/Collection.cs
+++ Climate Quest/Screens/Collection.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Text;
+using System.Collections.Generic;
 using ClimateQuest.GameTextures;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -8,8 +8,24 @@ namespace ClimateQuest.Screens;
 
 public class Collection : Screen
 {
+    private List<String> ClimateActions = new List<string>();
+    
     public Collection(ScreenManager manager, SpriteBatch spriteBatch, Rectangle box, SpriteFont font, string text,
         Color color, float scale, int padding, Texture2D background) : base(manager, spriteBatch)
+    {
+    }
+
+    public void AddClimateAction(String action)
+    {
+        ClimateActions.Add(action);
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        
+    }
+
+    public override void Draw(GameTime gameTime)
     {
         
     }
