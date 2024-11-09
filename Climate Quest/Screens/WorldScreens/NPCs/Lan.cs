820c1f657bab45c23c21c10f129615e44537a751
Vincent <vinkentclicker27@gmail.com>
2024-11-09 15:38:23 -0500

    added lan

--- Climate Quest/Screens/WorldScreens/NPCs/Lan.cs
+++ Climate Quest/Screens/WorldScreens/NPCs/Lan.cs
@@ -8,10 +8,17 @@ namespace ClimateQuest.Screens.WorldScreens.NPCs;
 public class Lan: Npc
 {
     public Lan(ScreenManager manager, SpriteBatch spriteBatch, World world) : base(manager, spriteBatch,
-        Color.Beige, world, null, new Point(4, 7), null,new List<Tuple<Color, string>>()
+        Color.Beige, world, new List<Tuple<Color, string>>()
         {
-            Tuple.Create(Color.LightBlue,"Some greater good indeed...")
-        })
+            Tuple.Create(Color.White, "Whoa, what's that blue bin?"),
+            Tuple.Create(Color.LightBlue,"That's a recycling bin."),
+            Tuple.Create(Color.White, "What's recycling?"),
+            Tuple.Create(Color.LightBlue, "The action or process of converting waste into reusable material!"),
+            Tuple.Create(Color.White, "What's the point of recycling?"),
+            Tuple.Create(Color.LightBlue, "Recycling reduces waste, conserves resources, saves energy, cuts pollution, and slows climate change!"),
+            Tuple.Create(Color.White, "I see! Thank you so much for the info!"),
+            Tuple.Create(Color.LightBlue, "No problem!"),
+        }, new Point(4, 7), null,null)
     {
 
     }
