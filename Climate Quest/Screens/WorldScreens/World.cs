aed1d0d842dce93a899ce95792db8157218f943a
Reieeeen <126581338+Reieeeen@users.noreply.github.com>
2024-11-09 12:55:29 -0500

    added splash text box and smog png

--- Climate Quest/Screens/WorldScreens/World.cs
+++ Climate Quest/Screens/WorldScreens/World.cs
@@ -13,7 +13,7 @@ public abstract class World: FullScreen
     protected Dictionary<Color,Npc> NPCs;
     
     protected Dictionary<Point,string> Exits;
-    public static readonly List<Color> BarrierColors = new() {Color.Red,Color.Brown,Color.SlateGray,Color.Gray};
+    public static readonly List<Color> BarrierColors = new() {Color.Red,Color.Brown,Color.SlateGray,Color.Gray,Color.Olive};
     public static readonly List<Color> VoidColors = new() { Color.Blue };
 
     protected readonly Tile[][] Tiles;
